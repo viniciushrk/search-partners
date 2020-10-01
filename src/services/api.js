6557ef3dc0eee26470dfdd1615370708745a468a
vinicius rosa <viniciusrosa444@gmail.com>
2020-10-01 15:17:17 -0400

    add push

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'https://api.github.com/',
   //baseURL: 'http://localhost:21000/',
-  baseURL: 'https://levoemcasa.com.br:21000',
+  baseURL: 'https://levoemcasa.com.br:21000/',
   headers:{
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDE1MDA0MTksImV4cCI6MzE3MTQ1OTQyODE5LCJzdWIiOiJzdXBlcnRlaWEifQ.T5r3TTNxVzKqroZ8--3aIGztDyO9Ya4Dxw4cgMhKh20"
   }
